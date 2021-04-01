5c8899474c8330b028cb23b643c2147bc27bdb6b
Coralie Baudry <coralie.bdy@gmail.com>
2021-04-01 15:51:37 +0200

    MAJ registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,20 +4,29 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager =$entityManager;
+    }
+
     /**
      * @Route("/registration", name="registration")
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
@@ -25,7 +34,11 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
-            dd($user);
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
         }
 
         return $this->render('registration/index.html.twig', [
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -8,11 +8,13 @@ use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegistrationType extends AbstractType
 {
@@ -21,19 +23,21 @@ class RegistrationType extends AbstractType
         $builder
             ->add('firstname', TextType::class, [
                 'label' => 'Votre prénom',
+                'constraints' => new  length(2, 2, 50),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre prénom'
                 ]
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Votre nom',
+                'constraints' => new  length(2, 2, 50),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre nom'
                 ]
             ])
             ->add('birthday', BirthdayType::class, [
                 'label' => 'Votre date de naissance',
-                'format' => 'dd-MM-yyyy'
+                'widget' => 'single_text'
             ])
             ->add('phone', TelType::class, [
                 'label' => 'Votre numéro de téléphone',
@@ -43,45 +47,56 @@ class RegistrationType extends AbstractType
             ])
             ->add('address', TextType::class, [
                 'label' => 'Votre adresse',
+                'constraints' => new  length(5, 5, 255),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre adresse'
                 ]
             ])
             ->add('postalCode', NumberType::class, [
                 'label' => 'Code postal',
+                'constraints' => new  length(5),
                 'attr' => [
                     'placeholder' => '99999'
                 ]
             ] )
             ->add('city', TextType::class, [
                 'label' => 'Ville',
+                'constraints' => new  length(3, 2, 100),
                 'attr' => [
                     'placeholder' => 'Merci de saisir votre ville'
                 ]
             ])
             ->add('username', TextType::class, [
                 'label' => 'Votre pseudo',
+                'constraints' => new  length(2, 2, 50),
                 'attr' => [
                     'placeholder' => 'merci de saisir votre pseudo'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Votre email',
+                'constraints' => new  length(5, 5, 100),
                 'attr' => [
                     'placeholder' => 'adresse@email.fr'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'les mots de passe doivent être identiques',
                 'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder' => 'merci de saisir votre mot de passe'
-                ]
-            ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmer votre mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'merci de confirmer votre mot de passe'
+                'required' => true,
+                'constraints' => new  length(6, 5, 15),
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre mot de passe'
+                    ]
                 ]
             ])
             ->add('cgv', null, [
